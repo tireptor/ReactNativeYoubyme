6a0d0ad9bb4645c7eb123a330253cd156a2689ca
boucherj <boucherj94@gmail.com>
2019-04-11 16:18:56 +0200

    a

--- Vue/Votation/Un_Soft_Skill.js
+++ Vue/Votation/Un_Soft_Skill.js
@@ -19,7 +19,7 @@ export default class Un_Soft_Skill extends React.Component {
     };
   }
 
-  ajoutPoint = (idSoftSkill, idPersonneVote, idUser, idPeriode) => {
+  ajoutPoint = (idSoftSkill, idPersonneVote, idUser, idPeriode, userVoteItems) => {
     fetch("http://192.168.43.206:1337/vote/voteUser" , {
       method: 'POST',
       headers: {
@@ -40,7 +40,8 @@ export default class Un_Soft_Skill extends React.Component {
             isLoaded: true,
             items: result
           }
-          );          
+          );
+          userVoteItems = 1 
         },
         (error) => {
           this.setState({
@@ -49,7 +50,7 @@ export default class Un_Soft_Skill extends React.Component {
           });
         }
       )
-    console.log("Ajout, id softSkill : " + idSoftSkill + " ID personne voté : " + idPersonneVote + " ID user: " + idUser + " ID periode: " + idPeriode);
+    console.log("Ajout, id softSkill : " + idSoftSkill + " ID personne voté : " + idPersonneVote + " ID user: " + idUser + " ID periode: " + idPeriode);  
   }
 
   retirePoint = (idSoftSkill, idPersonneVote, idUser, idPeriode) => {
@@ -63,7 +64,7 @@ export default class Un_Soft_Skill extends React.Component {
       <View  style={styles.container}>
         <Text style={styles.text}>{this.props.nom_t_personne}</Text>
         <View  style={styles.otherContainer}>
-          <TouchableOpacity style={styles.touchable} onPress={() => this.ajoutPoint(this.props.id_soft_skill, this.props.id_personne_vote, this.props.id_user, this.props.id_periode)}>            
+          <TouchableOpacity style={styles.touchable} onPress={() => this.ajoutPoint(this.props.id_soft_skill, this.props.id_personne_vote, this.props.id_user, this.props.id_periode, this.props.VoteUserItems)}>            
             <Text style={styles.button}>+</Text>                    
           </TouchableOpacity>
           <TouchableOpacity style={styles.touchable} onPress={() => this.retirePoint(this.props.id_soft_skill, this.props.id_personne_vote, this.props.id_user, this.props.id_periode)}>            
