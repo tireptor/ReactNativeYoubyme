57fd601de5df6d211338fed49f2409423cf310a7
boucherj <boucherj94@gmail.com>
2019-04-12 14:59:42 +0200

    Mot de passe (steak) caché

--- Vue/Login.js
+++ Vue/Login.js
@@ -142,7 +142,7 @@ export default class Login extends React.Component {
               value={this.state.Email}
             />
             <Text style={styles.title}>Mot de passe</Text>
-            <TextInput style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1, margin: '1%', textAlign:"center"}}
+            <TextInput secureTextEntry={true} style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1, margin: '1%', textAlign:"center"}}
               onChangeText={(Password) => this.setState({Password})} defaultValue = {'coucou'}
               value={this.state.Password}
             />
@@ -169,7 +169,7 @@ export default class Login extends React.Component {
           value={this.state.Email}
         />
         <Text style={styles.title}>Mot de passe</Text>
-        <TextInput style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1, margin: '1%', textAlign:"center"}}
+        <TextInput secureTextEntry={true} style={{height: 40, width: 300, borderColor: 'gray', borderWidth: 1, margin: '1%', textAlign:"center"}}
           onChangeText={(Password) => this.setState({Password})}
           value={this.state.Password}
         />
